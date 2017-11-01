8d021954451ea8a7e7db2c2425e673293821e5b3
TamSFr <tamashipoub@gmail.com>
2017-11-01 16:14:04 +0100

    on avance sur l aleatoire

--- prog.js
+++ prog.js
@@ -1,13 +1,47 @@
 'use strict';
 
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngSanitize']);
 
 app.controller('choix', 
 	function($scope) 
 	{
-		$scope.question = "Ca va ?";
+		/*Tableau Contenant toute les question*/
+		/*-------------------
+		--------Debut--------
+		---------------------*/
+		var tabQuest = [
+			"Fais un pierre feuille ciseau avec ton voisin de droite !<br>Le perdant bois 3coups !",
+			"Tu es le maitre du pouce, tu ne pourras le faire que 3fois",
+			"BTC",
+			"DTC"
+		];
+		/*-------------------
+		---------Fin---------
+		---------------------*/
+		
+		
+		/*Message de base*/
+		$scope.question = "Choisis un chiffre entre<br>1 et 9";
+		
+		
+		/*Fonction afficher :
+			Modifie la question de maniere aleatoire
+		  Variables : 
+		  	n : entier => memorise le nombre choisis par l'utilisateur*/
+		/*-------------------
+		--------Debut--------
+		---------------------*/
 		$scope.aff = function(n) {
-			$scope.question = n;
+			$scope.question = tabQuest[Math.floor(Math.random()*tabQuest.length)];
+			if($scope.question=="BTC"){
+				$scope.question = "Tu bois "+n+" coups !";
+			}
+			if($scope.question=="DTC"){
+				$scope.question = "Tu donnes "+n+" coups !";
+			}
 		};
+		/*-------------------
+		---------Fin---------
+		---------------------*/
 	}
 );
