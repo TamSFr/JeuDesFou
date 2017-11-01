4ba723997e01a74aa5f9218a5dc4467f050ff164
TamSFr <tamashino08@gmail.com>
2017-11-02 00:15:26 +0100

    ajout d un compteur

--- prog.js
+++ prog.js
@@ -5,50 +5,62 @@ var app = angular.module("myApp", ['ngSanitize']);
 app.controller('choix', 
 	function($scope, $timeout) 
 	{
-		//Tableau Contenant toute les question
-		/*-------------------
-		--------Debut--------
-		---------------------*/
-		var tabQuest = [
-			"Fais un pierre feuille ciseau avec ton voisin de droite !<br>Le perdant bois 3coups !",
-			"Tu es le maitre du pouce, tu ne pourras le faire que 3fois",
-			"BTC",
-			"DTC"
-		];
-		/*-------------------
-		---------Fin---------
-		---------------------*/
+		/*----------------------------------------------------------------------------
+		--------------------------                          --------------------------
+		-------------------------- SCOPE AFFICHAGE / BOUTON --------------------------
+		--------------------------                          --------------------------
+		------------------------------------------------------------------------------*/
 		
-		
-		/*Message de base*/
-		$scope.question = "Choisis un chiffre entre 1 et 9";
-		
-		
-		
-		/*Fonction afficher apres un temps :
-			Modifie la question de maniere aleatoire
-		  Variables : 
-		  	n : entier => memorise le nombre choisis par l'utilisateur*/
-		/*-------------------
-		--------Debut--------
-		---------------------*/
-		$scope.afficher = function(n)
-		{
-			$scope.question = tabQuest[Math.floor(Math.random()*tabQuest.length)];
-			if($scope.question=="BTC"){
-				$scope.question = "Tu bois "+n+" coups !";
+			//Tableau Contenant toute les question
+			/*-------------------
+			--------Debut--------
+			---------------------*/
+			var tabQuest = [
+				"Fais un pierre feuille ciseau avec ton voisin de droite !<br>Le perdant bois 3coups !",
+				"Tu es le maitre du pouce, tu ne pourras le faire que 3fois",
+				"BTC",
+				"DTC"
+			];
+			/*-------------------
+			---------Fin---------
+			---------------------*/
+			
+			
+			/*Message de base*/
+			$scope.question = "Choisis un chiffre entre 1 et 9";
+			
+			
+			
+			/*Fonction afficher apres un temps :
+				Modifie la question de maniere aleatoire
+			  Variables : 
+				n : entier => memorise le nombre choisis par l'utilisateur*/
+			/*-------------------
+			--------Debut--------
+			---------------------*/
+			$scope.afficher = function(n)
+			{
+				$scope.question = tabQuest[Math.floor(Math.random()*tabQuest.length)];
+				if($scope.question=="BTC"){
+					$scope.question = "Tu bois "+n+" coups !";
+				}
+				if($scope.question=="DTC"){
+					$scope.question = "Tu donnes "+n+" coups !";
+				}
 			}
-			if($scope.question=="DTC"){
-				$scope.question = "Tu donnes "+n+" coups !";
+			/*-------------------
+			---------Fin---------
+			---------------------*/
+			
+			//timeout avant le lancement de la fonction afficher
+			$scope.aff = function(n){
+				$timeout($scope.afficher(n), 10000);
 			}
-		}
-		/*-------------------
-		---------Fin---------
-		---------------------*/
-		
-		$scope.aff = function(n){
-			$timeout($scope.afficher(n), 10000);
-		}
+			
+			/*Nombre de queston estante
+			==> affiché en bas de page*/
+			$scope.questRest = "Questions restante : "+tabQuest.length;
+			
 		
 	}
 );
